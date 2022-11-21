d9ff8721fe754a7c683a33b76449f521c8b7e74a
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-11-22 01:49:05 +0300

    bind polyfill support

---
+++ js/bind_polyfill.js
@@ -0,0 +1,10 @@
+Function.prototype.bind = Function.prototype.bind || 
+    function(target){
+        let self = this
+        return function (args) {
+            if (!(args instanceof Array)) {
+                args = [args]
+            }
+            self.apply(target, args)
+        }
+    }
